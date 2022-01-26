948425eae6b8fcf1b5d49477128ea0c9cf66e8a9
Chahlyk <Lubimiekonfeti312@gmail.com>
2022-01-26 18:01:16 +0200

    moving layout view from the sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,11 @@ import { LayoutComponent } from './shared/layout/layout.component';
 const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(mod => mod.AuthModule) },
-  { path: 'pages', component: LayoutComponent, children: [
-      { path: '', redirectTo: '/bill', pathMatch: 'full' },
-      { path: 'bill', loadChildren: () => import('./pages/bill/bill.module').then(mod => mod.BillModule) },
-    ] },
+  // { path: 'pages', component: LayoutComponent, children: [
+  //     { path: '', redirectTo: '/bill', pathMatch: 'full' },
+  //     { path: 'bill', loadChildren: () => import('./pages/bill/bill.module').then(mod => mod.BillModule) },
+  //   ] },
+  { path: 'pages', component: LayoutComponent },
 ];
 
 @NgModule({
