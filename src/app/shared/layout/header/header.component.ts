e9572cf55492fce1695f6bfe0870f9f54bbe2a79
Chahlyk <lubimiekonfeti312@gmail.com>
2022-01-31 15:37:03 +0200

    finished header

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,9 +12,17 @@ export class HeaderComponent implements OnInit {
 
   public showState: boolean = true;
 
+  constructor(private router: Router) {
+  }
+
   public show(): void {
     this.showState = !this.showState;
     this.clickChange.emit(this.showState);
+  }
+
+  public exit(): void {
+    localStorage.removeItem('User');
+    this.router.navigate(['/sign-in']);
   }
 
   public ngOnInit(): void {
