ff1dfc80d5e91e2803b430903dda8eff6e7e3064
Chahlyk <lubimiekonfeti312@gmail.com>
2022-04-04 12:59:39 +0300

    created the view of table and buttons for add event/category

---
+++ src/app/pages/record/category-table/category-table.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { ICategories } from '../../history/history.interface';
+
+
+@Component({
+  selector: 'app-category-table',
+  templateUrl: './category-table.component.html',
+  styleUrls: ['./category-table.component.css']
+})
+export class CategoryTableComponent {
+
+  @Input() public dataSource: ICategories[] = [];
+
+  public displayedColumns: string[] = ['id', 'name', 'capacity', 'action'];
+
+}
--- src/app/pages/record/record.module.ts
+++ src/app/pages/record/record.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RecordComponent} from './record.component';
 import {RecordRoutingModule} from './record-routing.module';
+import { CategoryTableComponent } from './category-table/category-table.component';
+import { MaterialModule } from '../../shared/material.module';
 
 
 @NgModule({
   declarations: [
-    RecordComponent
+    RecordComponent,
+    CategoryTableComponent
   ],
   exports: [
     RecordComponent
   ],
-  imports: [
-    CommonModule,
-    RecordRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        RecordRoutingModule,
+        MaterialModule
+    ]
 })
 export class RecordModule { }
