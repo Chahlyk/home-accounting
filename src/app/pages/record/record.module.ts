b415902c05d6308343de2a8e61d67f3a4737f066
Катерина <81821659+Chahlyk@users.noreply.github.com>
2022-04-05 19:35:42 +0200

    Record module view (#4)
    * created service for getting, posting and editing data
    
    * changed type in the methods
    
    * created the view of table and buttons for add event/category
    
    * edit the style code
    
    * edit the style code
    
    * replace id with index
    
    * numeric row in table by index

--- src/app/pages/record/record.module.ts
+++ src/app/pages/record/record.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RecordComponent} from './record.component';
-import {RecordRoutingModule} from './record-routing.module';
+import { RecordComponent } from './record.component';
+import { RecordRoutingModule } from './record-routing.module';
+import { CategoryTableComponent } from './category-table/category-table.component';
+import { MaterialModule } from '../../shared/material.module';
 
 
 @NgModule({
   declarations: [
-    RecordComponent
+    RecordComponent,
+    CategoryTableComponent
   ],
   exports: [
     RecordComponent
   ],
-  imports: [
+    imports: [
     CommonModule,
-    RecordRoutingModule
-  ]
+    RecordRoutingModule,
+    MaterialModule
+    ]
 })
 export class RecordModule { }
